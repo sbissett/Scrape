56e117a20b47aa96f82e20914f64b745fb241382
Stephen Bissett <steve.bissett5@gmail.com>
2019-01-07 15:01:47 -0700

    indexmodels

--- controllers/webScrapperController.js
+++ controllers/webScrapperController.js
@@ -6,11 +6,11 @@ let db = require("../models"); // Require all models
 
 /////////////////////////////////////////////// /* Mongoose Configuration */ ////////////////////////////////////////////////////////
 mongoose.Promise = Promise; // Set mongoose to leverage Built in JavaScript ES6 Promises
-mongoose.connect("mongodb://heroku_n498q09l:nqhsgor6hvbhfudh35mk0npfo0@ds147267.mlab.com:47267/heroku_n498q09l", { // Connect to the Mongo DB
+mongoose.connect("mongodb://", { // Connect to the Mongo DB
   useMongoClient: true
 });
 
-// mongodb://heroku_n498q09l:nqhsgor6hvbhfudh35mk0npfo0@ds147267.mlab.com:47267/heroku_n498q09l
+// mongodb://heroku_need to include heroku link above after the forward slakses
 
 let mongooseConnection = mongoose.connection;
 
